30e486110a6b4e1d427b65a986900c4977b34376
satoyami <satoyami@yahoo.com>
2016-10-05 22:12:04 -0700

    model updates - need more hierarchy

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const workouts = require('./workouts');
 const Workout = require('./workoutModel');
 const moment = require('moment');
 
-// console.log(wkt.name);
-// console.log('--------------');
-// console.log(wkt.TYPE);
-// console.log('--------------');
-// console.log(wkt.PropNames);
-// console.log('--------------');
-// console.log(wkt.getReps());
 hopper.selectPick().then((result) => {
   let wkt = new Workout(workouts[result]);
   console.log('--------------');
   console.log('workout picked:');
   console.log(wkt);
+  console.log(`Total reps: ${wkt.getReps()}`);
 });
--- workoutModel.js
+++ workoutModel.js
@@ -29,17 +29,20 @@ class Workout {
       if (params.hasOwnProperty(props[prop])) this[prop] = params[props[prop]];
     });
 
-    this.name = params.name;
-    this.description = params.description;
-    this.timeScheme = params.timeScheme || 'For time';
-    this.averageTime = params.averageTime;
-    this.movements = params.movements || [];
-    this.repScheme = params.repScheme || [];
-    this.weights = params.weights || {};
+    this.name            = params.name;
+    this.description     = params.description;
+    this.timeScheme      = params.timeScheme || 'For time';
+    this.averageTime     = params.averageTime;
+    this.movements       = params.movements || [];
+    //TODO need better breakdown of reps for workouts with distances
+    //TODO breakdown timecapped workouts - (timecap / (time per movement * movements))
+    this.repScheme       = params.repScheme || [];
+    this.roundMultiplier = params.roundMultiplier || 0;
+    this.weights         = params.weights || {};
     this.linksToMovement = params.linksToMovement || [];
-    this.categories = params.categories || [];
-    this.warmUps = params.warmUps || [];
-    this.date = params.date || moment().format('YYYY-MM-DD_HH:MM:ss_dddd');
+    this.categories      = params.categories || [];
+    this.warmUps         = params.warmUps || [];
+    this.date            = params.date || moment().format('YYYY-MM-DD_HH:MM:ss_dddd');
   }
 
   /**
@@ -48,9 +51,10 @@ class Workout {
    * @returns {number}
    */
   getReps() {
-    return _.reduce(this.repScheme, (sum, n) => {
+    let totalReps = _.reduce(this.repScheme, (sum, n) => {
       return sum + n;
     }, 0);
+    return (this.roundMultiplier !== 0) ? totalReps * this.roundMultiplier : totalReps;
   }
 }
 
