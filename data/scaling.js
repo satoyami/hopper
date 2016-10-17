8022bc641a40f1cdcbad3965eff0ce81c6a2c03a
satoyami <satoyami@yahoo.com>
2016-10-17 05:51:47 -0700

    scaling skeleton

--- data/scaling.js
+++ data/scaling.js
@@ -1,32 +1,14 @@
-const moment = require('moment');
+/*
+ * Scaling options for movements
+ */
 
 module.exports = [
   {
-    name: '21 Guns',
-    description: '21 min 400m run, 21 push-ups, 21 box-jumps, 15 burpees, 9 pull-ups',
-    timeScheme: 'AMRAP',
-    averageTime: '21 minutes',
-    timeCap: true,
-    movements: ['run','push-ups','box-jumps','burpees','pull-ups'],
-    repScheme: [21,21,15,9],
-    distance: 400,
-    // roundMultiplier: ,
-    weights: {},
-    linksToMovement: ['http://wodwell.com/wod/amanda/'],
-    categories: ['gymanastics','olympic-lifting','barbell','rings','couplet'],
-    warmUps: ['kitchen-sink-stretch','shoulder-pass-throughs','air-squat','banded-OH-extension'],
+    name: 'deadlift',
+    scaling: ['kettelbell-deadlift','light-barbell','sumo-deadlift']
   },
   {
-    name: 'Alexander',
-    description: '5 rounds 31 back-squat, 12 power-cleans',
-    timeScheme: 'For Time',
-    averageTime: '10 minutes',
-    movements: ['back-squat','power-clean'],
-    repScheme: [31,12],
-    roundMultiplier: 5,
-    weights: {},
-    linksToMovement: ['http://wodwell.com/wod/amanda/'],
-    categories: ['gymanastics','olympic-lifting','barbell','rings','couplet'],
-    warmUps: ['kitchen-sink-stretch','shoulder-pass-throughs','air-squat','banded-OH-extension'],
+    name: 'clean-and-jerk',
+    scaling: ['dumbbell-clean-and-jerk']
   }
 ];
